470c12d571b22984611904279291b497f1f502d3
FluffMasterX <kobi.malul@gmail.com>
2020-01-13 15:16:30 +0200

    aaa

--- Assets/Scripts/NonObjectClasses/StateControllerClass.cs
+++ Assets/Scripts/NonObjectClasses/StateControllerClass.cs
@@ -6,15 +6,19 @@ public class StateControllerClass : MonoBehaviour
 {
     // Start is called before the first frame update
         private bool _playerCanMoveToThisState = false;
-        private bool _stateIsSelected() {
-            if (StateManager._stateManager.SelectedState.name == this.gameObject.name) {
-                return true;
+        public bool StateIsSelected() {
+            if (StateManager._stateManager.SelectedState != null) {
+                if (StateManager._stateManager.SelectedState.name == this.gameObject.name) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
             }
-            else {
-                return false;
-            }
+            else { return false;}
         }
         public bool PlayerCanMoveToThisState { get => _playerCanMoveToThisState ; set { _playerCanMoveToThisState = value;}}
         //public bool StateIsSelected {get => _stateIsSelected ; set {_stateIsSelected = value;}}
+        
     
 }
