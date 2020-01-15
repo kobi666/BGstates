c4a4b694202457407cf9cfee03e67da6c06cc160
FluffMasterX <kobi.malul@gmail.com>
2020-01-16 01:18:31 +0200

    kk

--- Assets/Scripts/PlayerActionController.cs
+++ Assets/Scripts/PlayerActionController.cs
@@ -4,25 +4,7 @@ using UnityEngine;
 
 public class PlayerActionController : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    MovementController _movementController;
     
-    private void Awake() {
-        _movementController = gameObject.GetComponent<MovementController>();
-    }
-    void Start()
-    {
-        TurnManager._turnManager.onTurnStarted += InitilizePlayerTurn;
-    }
 
-    private void InitilizePlayerTurn() {
-        _movementController.MovementPoints(true);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
